cad26fdbd5da1c5b18cf9ed6f8f23b97a3f7eb9d
LUIYI VARGAS <lvargas9715@gmail.com>
2023-03-09 00:14:02 -0400

    Project Structure
    feat: create, configure project and create folder structure

---
+++ AppLancerGroup/src/app/pages/public/signin/signin.page.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-signin',
+  templateUrl: './signin.page.html',
+  styleUrls: ['./signin.page.scss'],
+})
+export class SigninPage implements OnInit {
+
+  current_year: number = new Date().getFullYear();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
